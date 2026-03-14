51bce5c0e4b91568dca8065a1cbf62289245f71d
Max2408 <jobanputramahir@gmail.com>
2026-03-14 15:17:40 +0530

    Add Logout

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -82,6 +82,12 @@ exports.login = async (req, res) => {
     return res.status(200).json({ status : "Success" });
 }
 
+exports.logout = async (req, res) => {
+    res.clearCookie("jwt");
+
+    return res.status(200).json({ status: "Success" });
+}
+
 exports.editRole = async(req, res) => {
     if(!["admin", "primary-admin"].includes(req.user.role)) {
         return res.status(401).json({ status: "Unauthorized", message: "Admin Access Only." });
--- backend/routes/router/auth.js
+++ backend/routes/router/auth.js
@@ -7,6 +7,7 @@ const { protectedAuth } = require('../../middlewares/auth')
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
+router.post('/logout', auth.logout);
 router.post('/edit', protectedAuth, auth.editRole);
 
 module.exports = router;
